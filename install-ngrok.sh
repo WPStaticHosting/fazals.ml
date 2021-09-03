e0f010e1511d27b5b3c71a60e2f60fbdf1611c2f
fazalfarhan01 <fazal.farhan@gmail.com>
2021-09-03 15:46:50 +0530

    adding auth

--- install-ngrok.sh
+++ install-ngrok.sh
@@ -1,3 +1,5 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ./ngrok-stable-linux-amd64.zip
 rm ./ngrok-stable-linux-amd64.zip
+
+echo "6K9YGmrk8NAaxP29mNCWr_3iyRKPUWZSJmMvmqzuzGZ"
