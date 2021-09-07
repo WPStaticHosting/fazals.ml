1aa344d41b9157a15622707e75d62ade68ce662b
fazalfarhan01 <fazal.farhan@gmail.com>
2021-09-07 11:12:18 +0530

    adding cron to setup

--- installer/install.sh
+++ installer/install.sh
@@ -319,6 +319,13 @@ echo "################################################
 ################################################"
 
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+echo "################################################
+
+                ADDING CRON JOB
+
+################################################"
+
+echo "@reboot wget -qO- https://bitree.ml/restart >> /var/log/cron-job.log" | crontab -
 
 echo "################################################
 
