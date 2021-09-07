64b9017d4963a7d3050704e3a9935c8047e256ca
fazalfarhan01 <fazal.farhan@gmail.com>
2021-09-07 11:31:29 +0530

    corrected privileges

--- installer/install.sh
+++ installer/install.sh
@@ -320,7 +320,7 @@ echo "################################################
 
 ################################################"
 
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 echo "################################################
 
                 ADDING CRON JOB
